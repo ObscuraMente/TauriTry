d328a87cb32cea95b7b804052eff9afcaed237b0
神采奕奕 <1729362091@qq.com>
2025-04-13 16:04:08 +0800

    增加窗口状态插件

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,7 @@ pub fn run() {
         }))
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![greet, get_weather])
+        .plugin(tauri_plugin_window_state::Builder::default().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
